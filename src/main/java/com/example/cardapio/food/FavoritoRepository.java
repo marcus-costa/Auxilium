8b52a5e1058d2f3ebf326b2abd9bbf51ff8f4310
Mateus_Alves(KiraYagami) <136859578+KiraYagamiDark@users.noreply.github.com>
2024-07-01 09:35:52 -0300

    Update FavoritoRepository.java

--- src/main/java/com/example/cardapio/food/FavoritoRepository.java
+++ src/main/java/com/example/cardapio/food/FavoritoRepository.java
@@ -1,7 +1,12 @@
 package com.example.cardapio.food;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface  FavoritoRepository extends JpaRepository<Favorito, Long> {
 
+    @Query("SELECT f FROM favoritos f WHERE f.url = :url")
+    Favorito findByUrl(String url);
 }
