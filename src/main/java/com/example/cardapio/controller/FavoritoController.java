2d4a353c367e1e1fbe3b0566a7dd3c7f83aa43fd
Mateus_Alves(KiraYagami) <136859578+KiraYagamiDark@users.noreply.github.com>
2024-07-01 09:37:39 -0300

    Update FavoritoController.java
    Adicionado importações necessárias

--- src/main/java/com/example/cardapio/controller/FavoritoController.java
+++ src/main/java/com/example/cardapio/controller/FavoritoController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.cardapio.food.Favorito;
 import com.example.cardapio.food.FavoritoRepository;
